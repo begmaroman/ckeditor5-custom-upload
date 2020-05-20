51416b3ab84bdcb7e0edd41b983031bafe542778
begmaroman <r.behma@svitla.com>
2020-05-20 09:27:56 +0300

    minor fixes

--- src/adapter.js
+++ src/adapter.js
@@ -6,21 +6,25 @@ export default class Adapter {
     }
 
     upload() {
-        return this.getCredentials().then(this.uploadImage.bind(this));
+        this.loader.file
+            .then(this.getCredentials.bind(this))
+            .then(this.uploadImage.bind(this));
     }
 
     abort() {
-        if (this.xhr) this.xhr.abort();
+        if (this.xhr) {
+            this.xhr.abort();
+        }
     }
 
-    getCredentials() {
+    getCredentials(file) {
         return new Promise((resolve, reject) => {
+            const filename = file.name;
+            if (!filename) {
+                return reject('No filename found');
+            }
 
-            var filename = this.loader.file.name;
-
-            if (!filename) return reject('No filename found');
-
-            var xhr = new XMLHttpRequest();
+            let xhr = new XMLHttpRequest();
 
             xhr.withCredentials = true;
             xhr.open('GET', this.url + '?filename=' + filename, true);
@@ -30,34 +34,32 @@ export default class Adapter {
             xhr.addEventListener('error', err => reject('crederr'));
             xhr.addEventListener('abort', err => reject('credabort'));
             xhr.addEventListener('load', function () {
-                var res = xhr.response;
+                const res = xhr.response;
+                if (!res) {
+                    return reject('No response from s3 creds url');
+                }
 
-                if (!res) return reject('No response from s3 creds url');
-
-                resolve(res);
+                resolve(res, file);
             });
 
             xhr.send();
-
-        });
+        })
     }
 
-    uploadImage(s3creds) {
+    uploadImage(s3creds, file) {
         return new Promise((resolve, reject) => {
-
-            var data = new FormData();
-
-            for (var param in s3creds.params) {
+            let data = new FormData();
+            for (let param in s3creds.params) {
                 if (!s3creds.params.hasOwnProperty(param)) continue;
 
                 data.append(param, s3creds.params[param]);
             }
 
-            data.append('Content-Type', this.loader.file.type)
+            data.append('Content-Type', file.type)
 
-            data.append('file', this.loader.file);
+            data.append('file', file);
 
-            var xhr = this.xhr = new XMLHttpRequest();
+            let xhr = this.xhr = new XMLHttpRequest();
 
             xhr.withCredentials = false;
             xhr.responseType = 'document';
@@ -84,12 +86,14 @@ export default class Adapter {
                     return reject('NoLocation: No location in s3 POST response');
                 }
 
-                resolve({ default: this.mapUrl(info) });
+                resolve({default: this.mapUrl(info)});
             });
 
             if (xhr.upload) {
                 xhr.upload.addEventListener('progress', e => {
-                    if (!e.lengthComputable) return;
+                    if (!e.lengthComputable) {
+                        return;
+                    }
 
                     this.loader.uploadTotal = e.total;
                     this.loader.uploaded = e.loaded;
